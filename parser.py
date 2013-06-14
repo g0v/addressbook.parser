ca06ae8ffdc64238f1053bb3cbb555c6b8d49d59
Superbil <superbil@gmail.com>
2013-06-14 14:45:26 +0800

    實作解析字串

--- parser.py
+++ parser.py
@@ -7,11 +7,13 @@ infoURL = 'http://oid.nat.gov.tw/infobox1/personmain.jsp'
 data = url.urlopen(infoURL).read().decode('big5')
 
 # parser data inside NextLevel tag
+# NextLevel(\'l=\u9ad8\u96c4\u5e02,c=TW\',2,\'\u9ad8\u96c4\u5e02\')
 requestList = []
-for l in re.findall(r'NextLevel\(\'(.*?)\'\)', data, re.M):
-    requestList.append(l.split(','))
+for line in re.findall(r'NextLevel\((.*?)\)', data, re.M):
+    utf_line = line.encode('utf-8')
     if __debug__:
-        print l.split(',')
+        print "input %s: %s" % (type(utf_line), utf_line)
+    requestList.append(utf_line)
 
 # get strcut
 # sDn, sLevel, sTitle
