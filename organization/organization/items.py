f2bfb3397d29eb61080feb68e076f5fbce096b29
Superbil <superbil@gmail.com>
2013-08-21 01:55:19 +0800

    define items

--- organization/organization/items.py
+++ organization/organization/items.py
@@ -11,3 +11,50 @@ class OrganizationItem(Item):
     # name = Field()
     pass
 
+
+class DGPAItem(Item):
+    '''
+    Example:
+    {"expanded":false,
+    "classes":"folder",
+    "hasChildren":true,
+    "id":"100000000A",
+    "text":"國民大會(100000000A)",
+    "children":null}
+    '''
+    orgID = Field()
+    name = Field()
+    hasChildren = Field()
+    classes = Field()
+
+
+class DGPADetailItem(Item):
+    name = Field()
+    orgID = Field()
+    tel = Field()
+    fax = Field()
+    address = Field()
+    url = Field()
+    email = Field()
+    google_map = Field()
+
+
+class ShowdataItem(Item):
+
+    dn = Field()
+    level = Field()
+    title = Field()
+
+
+class OIDItem(Item):
+
+    oid = Field()
+    oid_name = Field()
+    dn = Field()
+    fax = Field()
+    tel = Field()
+    email = Field()
+    address = Field()
+    postal_address = Field()
+    up_gov = Field()
+    url = Field()
