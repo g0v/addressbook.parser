e9108263e54d4dbcecc6a6eb8d27f0cab6943bfb
Superbil <superbil@gmail.com>
2013-08-21 01:51:59 +0800

    init scrapy

---
+++ organization/organization/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for organization project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'organization'
+
+SPIDER_MODULES = ['organization.spiders']
+NEWSPIDER_MODULE = 'organization.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'organization (+http://www.yourdomain.com)'
