3a42a7836aca690447c393276adc259264c33b84
Superbil <superbil@gmail.com>
2013-08-21 03:41:32 +0800

    save data into one json file

--- organization/pipelines.py
+++ organization/pipelines.py
@@ -2,7 +2,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from json import dumps
+import codecs
 
-class OrganizationPipeline(object):
+class JsonWriterPipeline(object):
+
+    def __init__(self):
+        self.file_name = "../raw_data/oid.json"
+
     def process_item(self, item, spider):
+        """
+        Save data to json format, this will use file_name to save
+        """
+        with codecs.open(self.file_name, 'w', 'utf-8') as f:
+            f.write(dumps(item, ensure_ascii = False, indent=4))
+
         return item
